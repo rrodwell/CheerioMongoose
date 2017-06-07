1d76a75c3def42eec04bf48ff85cb665d0e6411b
Ryan Rodwell <ryanrodwell@gmail.com>
2017-06-06 21:42:33 -0400

    mlab connection

--- routing/routes.js
+++ routing/routes.js
@@ -6,8 +6,8 @@ var request = require("request");
 var cheerio = require("cheerio");
 
 // Requiring our Note and Article models
-var Note = require("../models/note.js");
-var Article = require("../models/article.js");
+var Note = require("../models/Note.js");
+var Article = require("../models/Article.js");
 
 
 
